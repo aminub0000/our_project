240d05d841ad071914aa68611bf2acecc4126870
Aminub <Image de profil  Amine Naboulsi naboulsiamine10062001@gmail.com>
2023-01-15 11:33:50 +0100

    adding files

---
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 825, 509);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController {
+    @FXML
+    Label welcomeText;
+    ImageView imageView;
+
+    @FXML
+    protected void mouseclcik() {
+    }
+
+}
